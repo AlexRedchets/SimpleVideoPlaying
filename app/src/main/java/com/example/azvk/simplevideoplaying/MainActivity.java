26f016745892e2e6be62ac1f4e3b44f4f0c19450
AlexRedchets <alexey.redchets@gmail.com>
2016-07-15 14:12:09 -0400

    Initial commit

---
+++ app/src/main/java/com/example/azvk/simplevideoplaying/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.azvk.simplevideoplaying;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Remove title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        //Remove notification bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+        final VideoView videoView = (VideoView)findViewById(R.id.videoView);
+        videoView.setVideoPath("http://www.quirksmode.org/html5/videos/big_buck_bunny.mp4");
+
+        //player controls
+        MediaController mediaController = new MediaController(this);
+        mediaController.setAnchorView(videoView);
+        videoView.setMediaController(mediaController);
+
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        android.widget.LinearLayout.LayoutParams params = (android.widget.LinearLayout.LayoutParams) videoView.getLayoutParams();
+        params.width =  metrics.widthPixels;
+        params.height = metrics.heightPixels;
+        params.leftMargin = 0;
+        videoView.setLayoutParams(params);
+
+        videoView.start();
+    }
+}
